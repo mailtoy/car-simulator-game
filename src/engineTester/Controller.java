5e7bcf1e2eb57d3961b4ec0f5920f8ac8eb0a061
mailtoy <mai_kms11@hotmail.com>
2019-10-30 18:54:21 +0900

    Add new window

--- src/engineTester/Controller.java
+++ src/engineTester/Controller.java
@@ -3,18 +3,18 @@ package engineTester;
 import connection.ClientType;
 
 public class Controller extends ClientType {
-	
+
 	public Controller() {
 		super();
 	}
-	
+
 	public static void main(String[] args) {
 		new Controller();
 	}
 
 	@Override
 	public void write(String message) {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 	}
 
 }
--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.plaf.basic.BasicArrowButton;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 import models.RawModel;
 import models.TexturedModel;
 import objConverter.ModelData;
@@ -23,7 +38,108 @@ import entities.Entity;
 import entities.Light;
 import entities.Player;
 
-public class MainGameLoop {
+public class MainGameLoop extends JFrame {
+
+	JFrame f;
+	public static JButton north;
+	public static JButton east;
+	public MainGameLoop() {
+		// ... Create components (but without listeners)
+		north = new JButton("North");
+		east = new JButton("East");
+		JButton south = new JButton("South");
+		JButton west = new JButton("West");
+		JButton center = new JButton("Center");
+		
+		// ... Create content pane, set layout, add components
+		JPanel content = new JPanel();
+		content.setLayout(new BorderLayout());
+
+		content.add(north, BorderLayout.NORTH);
+		content.add(east, BorderLayout.EAST);
+		content.add(south, BorderLayout.SOUTH);
+		content.add(west, BorderLayout.WEST);
+		content.add(center, BorderLayout.CENTER);
+
+		// ... Set window characteristics.
+		setContentPane(content);
+		setTitle("BorderTest");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		// JFrame frame = new JFrame("Arrow Button Demo");
+		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// frame.setLayout(new BorderLayout(5,5));
+		// frame.add(new BasicArrowButton(BasicArrowButton.EAST),
+		// BorderLayout.EAST);
+		// frame.add(new BasicArrowButton(BasicArrowButton.NORTH),
+		// BorderLayout.NORTH);
+		// frame.add(new BasicArrowButton(BasicArrowButton.SOUTH),
+		// BorderLayout.SOUTH);
+		// frame.add(new BasicArrowButton(BasicArrowButton.WEST),
+		// BorderLayout.WEST);
+		// frame.pack();
+		// frame.setSize(300, 300);
+		// frame.setVisible(true);
+
+		// f = new JFrame();
+		//
+		// JButton b1 = new JButton("1");
+		// JButton b2 = new JButton("2");
+		// JButton b3 = new JButton("3");
+		// JButton b4 = new JButton("4");
+		// JButton b5 = new JButton("5");
+		// JButton b6 = new JButton("6");
+		// JButton b7 = new JButton("7");
+		// JButton b8 = new JButton("8");
+		// JButton b9 = new JButton("9");
+		//
+		// f.add(b1);
+		// f.add(b2);
+		// f.add(b3);
+		// f.add(b4);
+		// f.add(b5);
+		// f.add(b6);
+		// f.add(b7);
+		// f.add(b8);
+		// f.add(b9);
+		//
+		// f.setLayout(new GridLayout(3, 3));
+		// setting grid layout of 3 rows and 3 columns
+		//
+		// f.setSize(300, 300);
+		// f.setVisible(true);
+		// GridBagLayout grid= new GridBagLayout();
+		// GridBagConstraints gbc = new GridBagConstraints();
+		// setLayout(grid);
+		// setTitle("GridBag Layout Example");
+		// GridBagLayout layout = new GridBagLayout();
+		// this.setLayout(layout);
+		// gbc.fill = GridBagConstraints.HORIZONTAL;
+		// gbc.gridx = 0;
+		// gbc.gridy = 0;
+		// gbc.gridwidth = 2;
+		// this.add(new Button("Button One"), gbc);
+		// gbc.gridx = 1;
+		// gbc.gridy = 0;
+		// this.add(new Button("Button two"), gbc);
+		// gbc.fill = GridBagConstraints.HORIZONTAL;
+		// gbc.ipady = 20;
+		// gbc.gridx = 0;
+		// gbc.gridy = 1;
+		// this.add(new Button("Button Three"), gbc);
+		// gbc.gridx = 1;
+		// gbc.gridy = 1;
+		// this.add(new Button("Button Four"), gbc);
+		// gbc.gridx = 0;
+		// gbc.gridy = 2;
+		// gbc.fill = GridBagConstraints.HORIZONTAL;
+		// gbc.gridwidth = 2;
+		// this.add(new Button("Button Five"), gbc);
+		// setSize(300, 300);
+		// setPreferredSize(getSize());
+		// setVisible(true);
+		// setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 	public static void main(String[] args) {
 
@@ -63,20 +179,30 @@ public class MainGameLoop {
 		Terrain terrain = new Terrain(0, 0, loader, new ModelTexture(loader.loadTexture("grass")));
 		Terrain terrain2 = new Terrain(1, 0, loader, new ModelTexture(loader.loadTexture("grass")));
 
-		
 		MasterRenderer renderer = new MasterRenderer();
 
 		RawModel bunnyModel = OBJLoader.loadObjModel("stanfordBunny", loader);
 		TexturedModel stanfordBunny = new TexturedModel(bunnyModel, new ModelTexture(loader.loadTexture("white")));
-		
-		//Vector3f(0, 0, -60) is position of bunny.
-		Player player = new Player(stanfordBunny, new Vector3f(0, 0, -60), 0, 180, 0, 0.6f);
-		Camera camera = new Camera(player);
 
+		// Vector3f(0, 0, -60) is position of bunny.
+		Player player = new Player(stanfordBunny, new Vector3f(0, 0, -40), 0, 180, 0, 0.6f);
+		Camera camera = new Camera(player);
+		JFrame window = new MainGameLoop();
+		window.setVisible(true);
 		
+		// MainGameLoop a = new MainGameLoop();
 		while (!Display.isCloseRequested()) {
 			camera.move();
 			player.move();
+			if (east.isSelected()) {
+				east.addActionListener(new ActionListener() {
+
+				    @Override
+				    public void actionPerformed(ActionEvent e) {
+				        player.checkInputsClick(1);
+				    }
+				});
+			}
 			renderer.processEntity(player);
 			renderer.processTerrain(terrain);
 			renderer.processTerrain(terrain2);
